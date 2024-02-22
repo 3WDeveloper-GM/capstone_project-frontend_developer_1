44e8cc3b836bde5356dd3cfb56125879797c24b0
3WDeveloper-GM <glopezmad@gmail.com>
2024-02-21 19:10:17 -0600

    created 2024/02/21-19:09

---
+++ js/index.js
@@ -0,0 +1,39 @@
+window.addEventListener("scroll", blur)
+
+function blur() {
+   var background = document.getElementById('background')
+   var scrollHeight = window.scrollY
+
+   var scrollMax = background.clientHeight
+
+   var opacity = (scrollHeight > scrollMax)
+      ? 0
+      : 1 - (scrollHeight / scrollMax)
+
+   background.style.opacity = opacity;
+
+   var blur = Math.floor(10 * (scrollHeight / scrollMax))
+
+   background.style.filter = `blur(${blur}px)`
+}
+
+
+const element = document.querySelector("#hello")
+const text = "Hello World"
+
+function textTyping(element, text, i = 0) {
+   if (i === 0) {
+      element.textContent = ""
+   }
+
+   element.textContent += text[i]
+
+   if (i === text.length - 1) {
+      return
+   }
+
+   setTimeout(() => textTyping(element, text, i + 1), 100)
+
+}
+
+textTyping(element, text)
