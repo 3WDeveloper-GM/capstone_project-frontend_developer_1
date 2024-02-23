38670df593467a5f4d337cece8990434c0f8fb37
3WDeveloper-GM <glopezmad@gmail.com>
2024-02-23 14:29:14 -0600

    made adjustments into the grid for the splash screen

--- js/index.js
+++ js/index.js
@@ -1,16 +1,15 @@
+//function section
+
 function textTyping(element, text, i = 0) {
    if (i === 0) {
       element.textContent = ""
    }
-
    element.textContent += text[i]
 
    if (i === text.length - 1) {
       return
    }
-
-   setTimeout(() => textTyping(element, text, i + 1), 100)
-
+   setTimeout(() => textTyping(element, text, i + 1), 150)
 }
 
 function blur() {
@@ -31,20 +30,35 @@ function blur() {
 }
 
 function unblur(currentBlur, currentOpacity, i = 0) {
+   if (i < 40) {
+      let opacityStep = 0.05
+      let blurStep = 2
+      let newOpacity = (currentOpacity < 1)
+         ? currentOpacity + opacityStep
+         : 1
+      let newBlur = (currentBlur > 0)
+         ? currentBlur - blurStep
+         : 0
 
-   var newOpacity = (currentOpacity < 1)
-      ? currentOpacity + 0.04
-      : 1
-   var newBlur = (currentBlur > 0)
-      ? currentBlur - 1
-      : 0
-   
-   var splash = document.getElementById('background')
+      let splash = document.getElementById('background')
 
-   splash.style.opacity = newOpacity
-   splash.style.filter = `blur(${newBlur}px)`
+      splash.style.opacity = newOpacity
+      splash.style.filter = `blur(${newBlur}px)`
 
-   setTimeout(() => unblur(newBlur, newOpacity,i + 1), 50)
+      setTimeout(() => unblur(newBlur, newOpacity, i + 1), 50)
+   }
+}
+
+function onLoadHandler() {
+   var background = document.getElementById('background');
+   var computedStyles = window.getComputedStyle(background);
+
+   var currentOpacity = computedStyles.opacity;
+   var currentBlur = computedStyles.filter;
+
+   var completeString = currentOpacity + " " + currentBlur
+   var numbers = ParseFloatsFromString(completeString)
+   unblur(numbers[1], numbers[0])
 }
 
 
@@ -58,25 +72,19 @@ function ParseFloatsFromString(stringArray) {
    return numbers
 }
 
+// Section that is executed at startup
 
-window.addEventListener('load', function () {
-   var background = document.getElementById('background');
-   var computedStyles = window.getComputedStyle(background);
-
-   var currentOpacity = computedStyles.opacity;
-   var currentBlur = computedStyles.filter;
-
-   var completeString = currentOpacity + " " + currentBlur
-   var numbers = ParseFloatsFromString(completeString)
-   unblur(numbers[1], numbers[0])
-})
+window.addEventListener('load', onLoadHandler, { once: true })
+//document.querySelector('blink-element').innerHTML += "|"
 
 window.addEventListener("scroll", blur)
 
 const element = document.querySelector("#hello")
-const text = "Hi there!"
+const text = "Hello there!"
 
+setTimeout(() => textTyping(element, text), 700)
 
-setTimeout(() => textTyping(element, text), 2000)
+const blink = document.getElementsByClassName('hello-there blink-element')
+blink[0].innerHTML += "|"
 
 
